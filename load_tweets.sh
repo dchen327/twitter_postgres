62bf326927b265807a2acca9bb84cc33e21b79eb
David Chen <davidc3287@gmail.com>
2025-04-07 21:20:31 -0700

    denorm

--- load_tweets.sh
+++ load_tweets.sh
@@ -13,10 +13,8 @@ for file in $files; do
     ./load_tweets.py --db "postgresql://postgres:pass@localhost:21026" --inputs "$file"
 done
 
-#echo 'load denormalized'
-#for file in $files; do
+echo 'load denormalized'
+for file in $files; do
     # use SQL's COPY command to load data into pg_denormalized
-#    unzip -p "$file" |
-#    sed 's/\\u0000//g' |
-#    psql "$DENORMALIZED_DB" -c "COPY $DENORMALIZED_TABLE ($DENORMALIZED_COLUMN) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
-#done
+    unzip -p "$file" | sed 's/\\u0000//g' | psql "postgresql://postgres:pass@localhost:11026" -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
+done
